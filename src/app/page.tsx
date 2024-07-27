d1e37989d7979623cab4de4207806e77bc413140
TheMystic07 <gurshabad90@gmail.com>
2024-07-27 14:15:13 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { CardHoverEffectDemo } from "@/components/CardHoverEffectDemo";
+import React from "react";
+// import { Vortex } from "./components/ui/vortex";
+
+export  default function page() {
+  return (
+    <div className="h-screen w-screen bg-black">
+
+  <CardHoverEffectDemo />
+    </div>
+  );
+}
