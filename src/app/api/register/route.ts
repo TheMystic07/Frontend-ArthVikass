cdcacea96ba056320aa603691f8333e541b0a32e
TheMystic07 <gurshabad90@gmail.com>
2024-07-28 12:29:37 +0530

    jesus ji da khalsa , holy spirit di fateh

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -17,3 +17,16 @@ export async function POST(req) {
     return NextResponse.json({ message: 'User creation failed', error: error.message }, { status: 500 });
   }
 }
+
+export async function GET(req) {
+  try {
+    await dbConnect();
+
+    const users = await User.find({});
+
+    return NextResponse.json(users, { status: 200 });
+  } catch (error) {
+    console.error("Error getting users:", error);
+    return NextResponse.json({ message: 'Error getting users', error: error.message }, { status: 500 });
+  }
+}
